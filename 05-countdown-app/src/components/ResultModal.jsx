0cc508fba32a1fe0cf41a81adffcb838f662de9e
Dimitar <dimikalapocev@gmail.com>
2024-06-26 19:40:12 +0200

    feat(component: ResultModal): display score and win/lose message

--- 05-countdown-app/src/components/ResultModal.jsx
+++ 05-countdown-app/src/components/ResultModal.jsx
@@ -1,20 +1,26 @@
 import { forwardRef } from 'react';
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, timeRemaining, onReset },
   ref
 ) {
+  const userLost = timeRemaining <= 0;
+  const polishedTimeRemaining = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
   return (
-    <dialog ref={ref} className='result-modal'>
-      <h2>Your {result}</h2>
+    <dialog ref={ref} className='result-modal' onClose={onReset}>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{' '}
+        <strong>{polishedTimeRemaining} seconds left.</strong>
       </p>
       <form method='dialog'>
-        <button>Close</button>
+        <button onClick={onReset}>Close</button>
       </form>
     </dialog>
   );
