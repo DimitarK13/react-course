6f09933191cd4ca58a6992c07e465c1064344237
Dimitar <dimikalapocev@gmail.com>
2024-06-29 22:07:36 +0200

    feat(component: App): display form/empty state dynamically

--- 06-management-app/src/App.jsx
+++ 06-management-app/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const [projects, setProjects] = useState({
+    selectedProject: undefined,
+    projectsArray: [],
+  });
+
+  function handleAddProject() {
+    setProjects((prevValues) => {
+      return {
+        ...prevValues,
+        selectedProject: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projects.selectedProject === null) {
+    content = <NewProject />;
+  } else if (projects.selectedProject === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddProject} />;
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
