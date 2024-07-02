f4722d8ea7de5bfd4e4b0dd66bab98112b436880
Dimitar <dimikalapocev@gmail.com>
2024-07-02 23:00:36 +0200

    feat(component: Quiz): complete state and shufle answers

--- 09-quiz-app/src/components/Quiz.jsx
+++ 09-quiz-app/src/components/Quiz.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react';
 import questions from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
+  const isQuizComplete = activeQuestionIndex === questions.length;
+
   function handleSelect(selectedAnswer) {
     setUserAnswers((prevValues) => [...prevValues, selectedAnswer]);
   }
+
+  if (isQuizComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='Colorful trophy' />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id='quiz'>
@@ -16,7 +31,7 @@ export default function Quiz() {
         <h2>{questions[activeQuestionIndex].text}</h2>
 
         <ul id='answers'>
-          {questions[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className='answer'>
               <button onClick={() => handleSelect(answer)}>{answer}</button>
             </li>
