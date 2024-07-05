f003200ee58b109891604c4c1154921a450818f4
Dimitar <dimikalapocev@gmail.com>
2024-07-05 23:49:51 +0200

    feat(component: AvailablePlaces): fetch data

--- 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
+++ 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    async function fetchPlaces() {
+      const res = await fetch('http://localhost:3000/places');
+      const resData = await res.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
+      title='Available Places'
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText='Fetching data...'
+      fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
   );
