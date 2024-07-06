c800d142d2aeb6423c8114b78f8466bce8e379f8
Dimitar <dimikalapocev@gmail.com>
2024-07-06 12:13:23 +0200

    feat: handle fetch errors

--- 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
+++ 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState();
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setIsFetching(true);
     async function fetchPlaces() {
-      const res = await fetch('http://localhost:3000/places');
-      const resData = await res.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const res = await fetch('http://localhost:3000/places');
+        const resData = await res.json();
+
+        if (!res.ok) throw new Error('Failed to fetch data');
+
+        setAvailablePlaces(resData.places);
+      } catch (err) {
+        setError({ message: err.message || 'Could not fetch places!' });
+      }
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title='An Error Occured' message={error.message} />;
+  }
 
   return (
     <Places
