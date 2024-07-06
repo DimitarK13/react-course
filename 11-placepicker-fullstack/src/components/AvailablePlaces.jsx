0a27d73f978c4da973a8e811c1865f507186f6cb
Dimitar <dimikalapocev@gmail.com>
2024-07-06 12:19:19 +0200

    feat(component: AvailablePlaces): sort places by user location

--- 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
+++ 11-placepicker-fullstack/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState();
@@ -16,11 +17,20 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
         if (!res.ok) throw new Error('Failed to fetch data');
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((pos) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            pos.coords.latitude,
+            pos.coords.longitude
+          );
+
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (err) {
         setError({ message: err.message || 'Could not fetch places!' });
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
 
     fetchPlaces();
