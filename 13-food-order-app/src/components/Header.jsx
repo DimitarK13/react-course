0acfd4e34b03f358eaa0889042ce0699adcb32a6
Dimitar <dimikalapocev@gmail.com>
2024-07-07 10:49:44 +0200

    fix(component: Header): semantic html tags

--- 13-food-order-app/src/components/Header.jsx
+++ 13-food-order-app/src/components/Header.jsx
@@ -2,13 +2,13 @@ import logo from '../assets/logo.jpg';
 
 export default function Header({ itemsInCart = 0 }) {
   return (
-    <div id='main-header'>
+    <header id='main-header'>
       <div id='title'>
         <img src={logo} alt='Logo' />
-        <h1>ReactFood</h1>
+        <h1>TheFoodShop</h1>
       </div>
 
       <button className='text-button'>Cart ({itemsInCart})</button>
-    </div>
+    </header>
   );
 }
