f2a310361e9e73a3b84f84ee78e3df98cf1b055d
Dimitar <dimikalapocev@gmail.com>
2024-07-07 15:24:30 +0200

    feat(component: Checkout): post http request

--- 13-food-order-app/src/components/Checkout.jsx
+++ 13-food-order-app/src/components/Checkout.jsx
@@ -13,25 +13,50 @@ export default function Checkout({ isCheckoutOpen, onHideCheckout }) {
     0
   );
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const fd = new FormData(e.target);
+    const data = Object.fromEntries(fd.entries());
+
+    const res = fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: data,
+        },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   return (
     <Modal open={isCheckoutOpen}>
-      <h2>Checkout</h2>
-      <p>Your Total: {formatPrices.format(totalPrice)}</p>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Your Total: {formatPrices.format(totalPrice)}</p>
 
-      <Input label='Full Name' type='text' id='full-name' />
-      <Input label='Email' type='email' id='email' />
-      <Input label='Street' type='text' id='street' />
-      <div className='control-row'>
-        <Input label='Postal Code' type='text' id='postal-code' />
-        <Input label='City' type='text' id='city' />
-      </div>
+        <Input label='Full Name' type='text' id='name' />
+        <Input label='Email' type='email' id='email' />
+        <Input label='Street' type='text' id='street' />
+        <div className='control-row'>
+          <Input label='Postal Code' type='text' id='postal-code' />
+          <Input label='City' type='text' id='city' />
+        </div>
 
-      <p className='modal-actions'>
-        <button type='button' className='text-button' onClick={onHideCheckout}>
-          Close
-        </button>
-        <button className='button'>Confirm Order</button>
-      </p>
+        <p className='modal-actions'>
+          <button
+            type='button'
+            className='text-button'
+            onClick={onHideCheckout}>
+            Close
+          </button>
+          <button className='button'>Confirm Order</button>
+        </p>
+      </form>
     </Modal>
   );
 }
