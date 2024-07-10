afd9a9aa1be22b086554a76b60a15cffc791dd62
Dimitar <dimikalapocev@gmail.com>
2024-07-10 18:55:55 +0200

    feat: navigation

--- 17-react-router/src/pages/Home.jsx
+++ 17-react-router/src/pages/Home.jsx
@@ -1,12 +1,7 @@
-import { Link } from 'react-router-dom';
-
 export default function Home() {
   return (
     <>
       <h1>My homepage</h1>
-      <p>
-        <Link to='/products'>All Products</Link>
-      </p>
     </>
   );
 }
--- 17-react-router/src/pages/Products.jsx
+++ 17-react-router/src/pages/Products.jsx
@@ -1,10 +1,7 @@
-import { Link } from 'react-router-dom';
-
 export default function Products() {
   return (
     <>
       <h1>Products Page</h1>
-      <Link to={'/'}>Homepage</Link>
     </>
   );
 }
