e54626444e3ca2e0488be651e319668ed282c0d6
Dimitar <dimikalapocev@gmail.com>
2024-07-10 19:16:24 +0200

    feat(page: Error)

---
+++ 17-react-router/src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import MainNavigation from '../components/MainNavigation';
+
+export default function Error() {
+  return (
+    <>
+      <MainNavigation />
+      <h1>An error occured</h1>
+      <p>Couldn't load page</p>
+    </>
+  );
+}
