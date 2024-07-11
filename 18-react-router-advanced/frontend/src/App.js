01bee6d85c53f456003af956229bcb360b781cf8
Dimitar <dimikalapocev@gmail.com>
2024-07-11 18:35:55 +0200

    feat(page: Events): load data using loader

--- 18-react-router-advanced/frontend/src/App.js
+++ 18-react-router-advanced/frontend/src/App.js
@@ -17,7 +17,19 @@ const router = createBrowserRouter([
         path: 'events',
         element: <EventsRoot />,
         children: [
-          { path: '', element: <EventsPage /> },
+          {
+            path: '',
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ':id', element: <EventDetailPage /> },
           { path: ':id/edit', element: <EditEventPage /> },
           { path: 'new', element: <NewEventPage /> },
--- 18-react-router-advanced/frontend/src/pages/EventsPage.jsx
+++ 18-react-router-advanced/frontend/src/pages/EventsPage.jsx
@@ -1,26 +1,11 @@
-import { Link, Outlet } from 'react-router-dom';
-import EventsNavigation from '../components/EventsNavigation';
+import { useLoaderData } from 'react-router-dom';
 
-const EVENTS = [
-  { id: 'p-01', title: 'Event 1' },
-  { id: 'p-02', title: 'Event 2' },
-  { id: 'p-03', title: 'Event 3' },
-  { id: 'p-04', title: 'Event 4' },
-  { id: 'p-05', title: 'Event 5' },
-];
+import EventsList from '../components/EventsList';
 
-export default function EventsPage() {
-  return (
-    <>
-      <h1>Events Page</h1>
-      <Outlet />
-      <ul>
-        {EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+function EventsPage() {
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
+
+export default EventsPage;
