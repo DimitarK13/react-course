879a2ca701dee805d0907f8471ebb46a2f6a23b3
Dimitar <dimikalapocev@gmail.com>
2024-07-12 17:08:00 +0200

    feat: delete event functionality

--- 18-react-router-advanced/frontend/src/App.js
+++ 18-react-router-advanced/frontend/src/App.js
@@ -3,12 +3,14 @@ import Homepage from './pages/Homepage';
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
 import EventDetailPage, {
   loader as eventsDetailLoader,
+  action as deleteEventAction,
 } from './pages/EventDetailPage';
 import EditEventPage from './pages/EditEventPage';
-import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
+import NewEventPage from './pages/NewEventPage';
 import Root from './pages/Root';
 import EventsRoot from './pages/EventsRoot';
 import Error from './pages/Error';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   {
@@ -32,20 +34,27 @@ const router = createBrowserRouter([
             loader: eventsDetailLoader,
             children: [
               {
-                path: '',
+                index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
-              { path: 'edit', element: <EditEventPage /> },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
       },
     ],
   },
 ]);
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
   return <RouterProvider router={router} />;
--- 18-react-router-advanced/frontend/src/pages/EventDetailPage.jsx
+++ 18-react-router-advanced/frontend/src/pages/EventDetailPage.jsx
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from 'react-router-dom';
+import { json, redirect, useRouteLoaderData } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 
 export default function EventDetailPage() {
@@ -9,7 +9,7 @@ export default function EventDetailPage() {
   return <EventItem event={event} />;
 }
 
-export const loader = async ({ _, params }) => {
+export const loader = async ({ params }) => {
   const id = params.id;
 
   const res = await fetch(`http://localhost:8080/events/${id}`);
@@ -18,4 +18,16 @@ export const loader = async ({ _, params }) => {
     throw json({ message: 'Could not fetch data for event' }, { status: 500 });
 
   return res;
+};
+
+export const action = async ({ params }) => {
+  const id = params.id;
+  const res = await fetch(`http://localhost:8080/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!res.ok)
+    throw json({ message: 'Could not delete event' }, { status: 500 });
+
+  return redirect('/events');
 };
