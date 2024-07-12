5a60a8e6d88e15228fa68f0be781decda460cd2f
Dimitar <dimikalapocev@gmail.com>
2024-07-12 18:03:56 +0200

    feat(page: events): defer data fetching

--- 18-react-router-advanced/frontend/src/pages/EventsPage.jsx
+++ 18-react-router-advanced/frontend/src/pages/EventsPage.jsx
@@ -1,26 +1,35 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
   const data = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={data.events}>
+        {(events) => <EventsList events={events} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok)
     throw json({ message: 'Could not load events!' }, { status: 500 });
 
-  return response;
+  const resData = await response.json();
+
+  return resData.events;
+};
+
+export const loader = () => {
+  return defer({
+    events: loadEvents(),
+  });
 };
