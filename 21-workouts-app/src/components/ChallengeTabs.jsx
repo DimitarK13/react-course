a1f2dae1be8b985fd87322a541be0ba06b19c82b
Dimitar <dimikalapocev@gmail.com>
2024-07-15 19:55:53 +0200

    feat: animate app

--- 21-workouts-app/src/components/ChallengeTabs.jsx
+++ 21-workouts-app/src/components/ChallengeTabs.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Badge from './Badge.jsx';
 
 function Tab({ isSelected, onSelect, badgeCaption, children }) {
@@ -5,12 +6,13 @@ function Tab({ isSelected, onSelect, badgeCaption, children }) {
     <li>
       <button
         className={isSelected ? 'selected' : undefined}
-        onClick={onSelect}
-      >
+        onClick={onSelect}>
         {children}
-        <Badge caption={badgeCaption}></Badge>
+        <Badge key={badgeCaption} caption={badgeCaption}></Badge>
       </button>
-      {isSelected && <div className="active-tab-indicator" />}
+      {isSelected && (
+        <motion.div layoutId='tab-indicator' className='active-tab-indicator' />
+      )}
     </li>
   );
 }
@@ -23,26 +25,23 @@ export default function ChallengeTabs({
 }) {
   return (
     <>
-      <menu id="tabs">
+      <menu id='tabs'>
         <Tab
           isSelected={selectedType === 'active'}
           onSelect={() => onSelectType('active')}
-          badgeCaption={challenges.active.length}
-        >
+          badgeCaption={challenges.active.length}>
           Active
         </Tab>
         <Tab
           isSelected={selectedType === 'completed'}
           onSelect={() => onSelectType('completed')}
-          badgeCaption={challenges.completed.length}
-        >
+          badgeCaption={challenges.completed.length}>
           Completed
         </Tab>
         <Tab
           isSelected={selectedType === 'failed'}
           onSelect={() => onSelectType('failed')}
-          badgeCaption={challenges.failed.length}
-        >
+          badgeCaption={challenges.failed.length}>
           Failed
         </Tab>
       </menu>
--- 21-workouts-app/src/components/Modal.jsx
+++ 21-workouts-app/src/components/Modal.jsx
@@ -1,13 +1,24 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <div className='backdrop' onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { y: 30, opacity: 0 },
+          visible: { y: 0, opacity: 1 },
+          exit: { y: 30, opacity: 0 },
+        }}
+        initial='hidden'
+        animate='visible'
+        exit='exit'
+        open
+        className='modal'>
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
