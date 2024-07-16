01b3b3b285c113a4b76b6c60b7d2afee41b33d0c
Dimitar <dimikalapocev@gmail.com>
2024-07-16 12:54:17 +0200

    feat: accordions

--- 22-patterns-and-best-practices/src/App.jsx
+++ 22-patterns-and-best-practices/src/App.jsx
@@ -1,5 +1,41 @@
+import Accordion from './components/Accordions/Accordion';
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why Work With Us?</h2>
+
+        <Accordion className='accordion'>
+          <Accordion.Item
+            id='accordion-1'
+            className='accordion-item'
+            title='We got 20 years of experience'>
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the bussiness of planning highly vacations trips for
+                more than 20 years
+              </p>
+            </article>
+          </Accordion.Item>
+
+          <Accordion.Item
+            id='accordion-2'
+            className='accordion-item'
+            title='We are working with local guides'>
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the bussiness of planning highly vacations trips for
+                more than 20 years
+              </p>
+            </article>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
